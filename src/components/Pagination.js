ead8547de8e0af5bcbcdee0f154edc7c87dacfbb
Jonathan Martins <dejimartins99@gmail.com>
2022-10-31 23:47:23 +0100

    accessibility - focusScope

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { FocusScope } from "@react-aria/focus";
 
 const Pagination = ({ paginate, handlePrevious, handleNext, pageNumber }) => {
   let pageNumbers = [];
@@ -9,36 +10,44 @@ const Pagination = ({ paginate, handlePrevious, handleNext, pageNumber }) => {
 
   return (
     <div className="pagination">
-      <button
-        className="direction"
-        disabled={pageNumber === 1 ? true : false}
-        onClick={(event) => handlePrevious(event)}
-      >
-        <FiChevronLeft size={20} />
-      </button>
-      <nav>
-        <ul className="pages">
-          {pageNumbers.map((number) => (
-            <li key={number} className="page_item">
-              <button
-                onClick={() => paginate(number)}
-                style={
-                  pageNumber === number ? { backgroundColor: "gainsboro" } : {}
-                }
-              >
-                {number}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <button
-        className="direction"
-        disabled={pageNumber === 10 ? true : false}
-        onClick={(event) => handleNext(event)}
-      >
-        <FiChevronRight size={20} />
-      </button>
+      <FocusScope>
+        <button
+          className="direction"
+          disabled={pageNumber === 1 ? true : false}
+          onClick={(event) => handlePrevious(event)}
+        >
+          <FiChevronLeft size={20} />
+        </button>
+        <nav>
+          <ul className="pages">
+            {pageNumbers.map((number) => (
+              <li key={number} className="page_item">
+                <button
+                  onClick={() => paginate(number)}
+                  style={
+                    pageNumber === number
+                      ? {
+                          backgroundColor: "gainsboro",
+                          color: "goldenrod",
+                          fontWeight: "bold",
+                        }
+                      : {}
+                  }
+                >
+                  {number}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <button
+          className="direction"
+          disabled={pageNumber === 10 ? true : false}
+          onClick={(event) => handleNext(event)}
+        >
+          <FiChevronRight size={20} />
+        </button>
+      </FocusScope>
     </div>
   );
 };
